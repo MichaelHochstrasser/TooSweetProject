e2b980bbe42270a32e04b41ac241fe8289b89ec6
Michael <michi.ho@gmx.ch>
2018-01-28 00:21:11 +0100

    imBut

--- app/src/main/java/com/example/michaelh/toosweetproject/ReceiptActivity.java
+++ app/src/main/java/com/example/michaelh/toosweetproject/ReceiptActivity.java
@@ -64,7 +64,7 @@ public class ReceiptActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(this.getApplicationContext(), "Hi", 10);
         toast.show();
 
-        btnRefresh = (ImageButton)findViewById(R.id.btnRefresh);
+        btnRefresh = (ImageButton)findViewById(R.id.imgBotRefresh);
         btnRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
